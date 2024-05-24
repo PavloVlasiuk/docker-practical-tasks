b61290202e1bf73bfde1b92f7ad749bf50674cec
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-05-24 18:13:53 +0300

    feat: added endpoint to multiply matrices

--- python-project/spaceship/routers/api.py
+++ python-project/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -11,3 +12,16 @@ def hello_world() -> dict:
 @router.get('/name')
 def get_student() -> dict:
     return {'name': 'Pavlo'}
+
+
+@router.get('/matrices')
+def get_multiplied_matrices() -> dict:
+    matrix_a = np.random.randint(0, 10, (10, 10)).tolist()
+    matrix_b = np.random.randint(0, 10, (10, 10)).tolist()
+    product = np.dot(matrix_a, matrix_b).tolist()
+    response = {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
+    }
+    return response
