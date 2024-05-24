0397dae35e4a953d96b2e5407297995e7da9ff2a
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-05-24 16:27:18 +0300

    feat: added new endpoint

--- python-project/spaceship/routers/api.py
+++ python-project/spaceship/routers/api.py
@@ -6,3 +6,8 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get('/name')
+def get_student() -> dict:
+    return {'name': 'Pavlo'}
